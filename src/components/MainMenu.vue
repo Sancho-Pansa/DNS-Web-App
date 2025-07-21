94328e670791cc5cdf9cf3d359d1ecb2cca9672a
SanchoPansa <zelgadis4@gmail.com>
2025-07-21 23:37:50 +0300

    PrimeVue; removed boilerplate files

---
+++ src/components/MainMenu.vue
@@ -0,0 +1,9 @@
+<script lang="ts">
+
+</script>
+
+<template>
+  <section>
+    
+  </section>
+</template>
---
+++ src/views/RecordView.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import MainMenu from "../components/MainMenu.vue"
+</script>
+
+<template>
+  <main>
+    <main-menu>
+
+    </main-menu>
+  </main>
+</template>
