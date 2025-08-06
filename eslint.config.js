e88d66e9eec9d7765b00751160290c574dbc2a8d
SanchoPansa <zelgadis4@gmail.com>
2025-08-06 13:17:00 +0300

    Established ESLint rules
    With eslint-plugin-vue and Stylistic ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -7,11 +7,21 @@ import stylistic from "@stylistic/eslint-plugin";
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
-    languageOptions: { globals: {...globals.browser, ...globals.node} }
+    languageOptions: { globals: { ...globals.browser, ...globals.node } }
   },
   // @ts-expect-error "It works anyway"
   tseslint.configs.recommended,
   pluginVue.configs["flat/essential"],
+  pluginVue.configs["flat/strongly-recommended"],
+  stylistic.configs.customize({
+    "indent": 2,
+    "quotes": "double",
+    "severity": "warn",
+    "semi": true,
+    "commaDangle": "only-multiline",
+    "quoteProps": "consistent",
+    "braceStyle": "1tbs"
+  }),
   { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
   {
     plugins: {
@@ -21,11 +31,28 @@ export default defineConfig([
       // ESLint-specific
       "@typescript-eslint/no-unused-vars": ["warn"],
 
-      // ESLint-Stylistic
-      "space-before-function-paren": ["error", {
+      // ESLint-Stylistic-specific
+      "@stylistic/space-before-function-paren": ["error", {
         "anonymous": "never",
         "named": "never",
         "asyncArrow": "always"
+      }],
+      "@stylistic/member-delimiter-style": ["warn", {
+        "multiline": {
+          "delimiter": "comma",
+          "requireLast": false
+        }
+      }],
+
+      // Vue-specific
+      "vue/max-attributes-per-line": ["warn", {
+        "singleline": {
+          "max": 2
+        }
+      }],
+      "vue/html-closing-bracket-newline": ["warn", {
+        "multiline": "never",
+        "selfClosingTag": { "multiline": "always" }
       }]
     }
   }
