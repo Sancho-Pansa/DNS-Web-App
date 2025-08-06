4ec550b528cfeed14139136ec86f211c7421f93b
SanchoPansa <zelgadis4@gmail.com>
2025-08-06 13:11:30 +0300

    Move out Dialog component
    All references in ControlBar changed to new generalized DnsRecordDialog

--- src/components/ControlBar.vue
+++ src/components/ControlBar.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-import { Button, Dialog, InputText, Checkbox, Message, Toast, useToast } from "primevue";
-import { Form, type FormSubmitEvent } from "@primevue/forms";
-import { zodResolver } from "@primevue/forms/resolvers/zod";
-import { z } from "zod";
-import { computed, ref } from "vue";
+import { ref } from "vue";
+import { Button, Toast, useToast } from "primevue";
+import { type FormSubmitEvent } from "@primevue/forms";
 import useDnsApi from "@/composables/DnsApi";
 import useRecordStore from "@/composables/useRecordStore";
 import { AxiosError } from "axios";
+import DnsRecordDialog from "@/components/DnsRecordDialog.vue";
 
 const initialValues = ref({
   hostname: "",
@@ -19,23 +18,25 @@ const editRecordVisible = ref(false);
 const hostname = ref("");
 const ipAddress = ref("");
 const addPtr = ref(true);
-const fqdn = computed(() => hostname.value ? `${hostname.value}.${import.meta.env.VITE_DEFAULT_ZONE}.` : "");
-
-const resolver = ref(zodResolver(
-  z.object({
-    hostname: z.hostname({ error: "Not a valid hostname!" }),
-    ipAddress: z.ipv4({ error: "Input is not the corrent IP Address!" })
-  })
-));
 
 const toast = useToast();
 
 const { addRecord } = useDnsApi();
 
-async function onFormSubmit(fse: FormSubmitEvent) {
+async function onAddSubmit(fse: FormSubmitEvent) {
   if (fse.valid) {
+    toast.add({
+      severity: "success",
+      summary: "Validation successful",
+      life: 2000
+    });
     try {
       await addRecord(hostname.value, ipAddress.value, addPtr.value);
+      toast.add({
+        severity: "success",
+        summary: "Record added",
+        life: 2000
+      });
     } catch (e: unknown) {
       if (e instanceof AxiosError) {
         generateErrorToast(`HTTP ${e.code}: ${e.message}`);
@@ -45,19 +46,7 @@ async function onFormSubmit(fse: FormSubmitEvent) {
         generateErrorToast("Unknown error!");
       }
     }
-    toast.add({
-      severity: "success",
-      summary: "Validation successful",
-      life: 2000
-    });
     newRecordVisible.value = false;
-  }
-
-  function generateErrorToast(message: string) {
-    toast.add({
-      severity: "error",
-      summary: message
-    });
   }
 }
 
@@ -68,92 +57,86 @@ function onEditClick() {
   ipAddress.value = selectedRecord.value?.ipAddress ?? "";
 }
 
+async function onEditSubmit(fse: FormSubmitEvent) {
+  if (fse.valid) {
+    toast.add({
+      severity: "success",
+      summary: "Validation successful",
+      life: 2000
+    });
+    try {
+      await addRecord(hostname.value, ipAddress.value, addPtr.value);
+      toast.add({
+        severity: "success",
+        summary: "Record added",
+        life: 2000
+      });
+    } catch (e: unknown) {
+      if (e instanceof AxiosError) {
+        generateErrorToast(`HTTP ${e.code}: ${e.message}`);
+      } else if (e instanceof Error) {
+        generateErrorToast(e.message);
+      } else {
+        generateErrorToast("Unknown error!");
+      }
+    }
+  }
+}
+
 function onDeleteClick() {
 
 }
+
+function generateErrorToast(message: string) {
+  toast.add({ severity: "error", summary: message });
+}
+
 </script>
 <template>
-  <Toast position="bottom-center" severity="error" />
-  <Button label="Новая запись" @click="newRecordVisible = true">
+  <Toast
+    position="bottom-center"
+    severity="error"
+  />
+  <Button
+    label="Новая запись"
+    @click="newRecordVisible = true">
     <slot name="icon">
-      <img src="@/assets/add.svg" alt="plus" />
+      <img src="@/assets/add.svg" alt="plus">
       Новая запись
     </slot>
   </Button>
-  <Button label="Изменить запись" @click="onEditClick">
+  <Button
+    label="Изменить запись"
+    @click="onEditClick">
     <slot name="icon">
-      <img src="@/assets/edit.svg" alt="edit" />
+      <img src="@/assets/edit.svg" alt="edit">
       Изменить запись
     </slot>
   </Button>
-  <Button label="Удалить запись" @click="onDeleteClick">
+  <Button
+    label="Удалить запись"
+    @click="onDeleteClick">
     <slot name="icon">
-      <img src="@/assets/delete.svg" alt="delete" />
+      <img src="@/assets/delete.svg" alt="delete">
       Удалить запись
     </slot>
   </Button>
-
-  <Dialog v-model:visible="newRecordVisible" header="Добавить A-запись в DNS" modal class="w-1/3">
-    <Form v-slot="$form" :resolver="resolver" :initial-values @submit="onFormSubmit" class="flex flex-col gap-4 w-full">
-      <div class="flex items-center gap-4">
-        <label for="hostname" class="font-bold w-24 self-baseline">Hostname</label>
-        <div class="flex flex-col flex-auto">
-          <InputText v-model="hostname" name="hostname" autocomplete="off" class="flex-auto h-8" />
-          <Message v-if="$form.hostname?.invalid" severity="error" size="small" variant="simple">
-            {{ $form.hostname.error.message }}
-          </Message>
-        </div>
-      </div>
-      <div class="flex items-center gap-4">
-        <label for="hostname" class="font-bold w-24 self-baseline">FQDN</label>
-        <InputText v-model="fqdn" id="hostname" class="flex-auto h-8 p-0" readonly disabled />
-      </div>
-      <div class="flex items-center gap-4">
-        <label for="ipAddress" class="font-bold w-24 self-baseline">IP Address</label>
-        <div class="flex flex-col flex-auto">
-          <InputText v-model="ipAddress" id="ipAddress" name="ipAddress" class="flex-auto h-8 p-0" autocomplete="off">
-            {{  }}
-          </InputText>
-          <Message v-if="$form.ipAddress?.invalid" severity="error" size="small" variant="simple">
-            {{ $form.ipAddress.error.message }}
-          </Message>
-        </div>
-      </div>
-      <div class="flex items-center gap-4">
-        <label for="addPtr" class="font-bold w-24">Add PTR record</label>
-        <Checkbox v-model="addPtr" id="addPtr" binary />
-      </div>
-      <Button type="submit" severity="secondary" label="Add"></Button>
-    </Form>
-  </Dialog>
-
-  <Dialog v-model:visible="editRecordVisible" header="Изменить A-запись в DNS" modal class="w-1/3">
-    <Form v-slot="$form" :resolver="resolver" :initial-values @submit="onFormSubmit" class="flex flex-col gap-4 w-full">
-      <label for="hostname" class="font-bold w-24 self-baseline">Hostname</label>
-        <div class="flex flex-col flex-auto">
-          <InputText v-model="hostname" name="hostname" autocomplete="off" class="flex-auto h-8" />
-          <Message v-if="$form.hostname?.invalid" severity="error" size="small" variant="simple">
-            {{ $form.hostname.error.message }}
-          </Message>
-        </div>
-      <div class="flex items-center gap-4">
-        <label for="hostname" class="font-bold w-24 self-baseline">FQDN</label>
-        <InputText v-model="fqdn" id="hostname" class="flex-auto h-8 p-0" readonly disabled />
-      </div>
-      <div class="flex items-center gap-4">
-        <label for="ipAddress" class="font-bold w-24 self-baseline">IP Address</label>
-        <div class="flex flex-col flex-auto">
-          <InputText v-model="ipAddress" id="ipAddress" name="ipAddress" class="flex-auto h-8 p-0" autocomplete="off" />
-          <Message v-if="$form.ipAddress?.invalid" severity="error" size="small" variant="simple">
-            {{ $form.ipAddress.error.message }}
-          </Message>
-        </div>
-      </div>
-      <div class="flex items-center gap-4">
-        <label for="addPtr" class="font-bold w-24">Add PTR record</label>
-        <Checkbox v-model="addPtr" id="addPtr" binary />
-      </div>
-      <Button type="submit" severity="secondary" label="Confirm"></Button>
-    </Form>
-  </Dialog>
+  <DnsRecordDialog
+    header="Добавить A-запись в DNS"
+    v-model:visible="newRecordVisible"
+    v-model:hostname="hostname"
+    v-model:ip-address="ipAddress"
+    v-model:add-ptr="addPtr"
+    :initial-values
+    @submit="onAddSubmit"
+  />
+  <DnsRecordDialog
+    header="Изменить A-запись в DNS"
+    v-model:visible="editRecordVisible"
+    v-model:hostname="hostname"
+    v-model:ip-address="ipAddress"
+    v-model:add-ptr="addPtr"
+    :initial-values
+    @submit="onEditSubmit"
+  />
 </template>
---
+++ src/components/DnsRecordDialog.vue
@@ -0,0 +1,125 @@
+<script setup lang="ts">
+import { InputText, Message, Dialog, Button, Checkbox } from "primevue";
+import { Form, type FormSubmitEvent } from "@primevue/forms";
+import { computed, ref } from "vue";
+import { zodResolver } from "@primevue/forms/resolvers/zod";
+import { z } from "zod";
+
+const visible = defineModel<boolean>("visible", { default: false });
+const hostname = defineModel<string>("hostname", { required: true });
+const ipAddress = defineModel<string>("ipAddress", { required: true });
+const addPtr = defineModel<boolean>("addPtr", { default: true });
+
+const fqdn = computed(() => hostname.value ? `${hostname.value}.${import.meta.env.VITE_DEFAULT_ZONE}.` : "");
+
+const emit = defineEmits<{
+  (event: "submit", payload: FormSubmitEvent<Record<string, unknown>>): void
+}>();
+
+const props = defineProps<{
+  header: string,
+  initialValues: {
+    hostname: string,
+    ipAddress: string,
+    addPtr: boolean
+  }
+}>();
+
+const resolver = ref(zodResolver(
+  z.object({
+    hostname: z.hostname({ error: "Not a valid hostname!" }),
+    ipAddress: z.ipv4({ error: "Input is not the correct IP Address!" })
+  })
+));
+
+function onSubmit(event: FormSubmitEvent<Record<string, unknown>>) {
+  emit("submit", event);
+}
+
+function onShow() {
+  hostname.value = props.initialValues.hostname;
+  ipAddress.value = props.initialValues.ipAddress;
+  addPtr.value = true;
+}
+</script>
+
+<template>
+  <Dialog
+    v-model:visible="visible"
+    :header="header"
+    modal
+    class="w-1/3"
+    @show="onShow">
+    <Form
+      v-slot="$form"
+      :resolver="resolver"
+      :initial-values
+      @submit="onSubmit"
+      class="flex flex-col gap-4 w-full">
+      <div class="flex items-center gap-4">
+        <label
+          for="hostname"
+          class="font-bold w-24 self-baseline">
+          Hostname
+        </label>
+        <div class="flex flex-col flex-auto">
+          <InputText
+            v-model="hostname"
+            name="hostname"
+            autocomplete="off"
+            class="flex-auto h-8"
+          />
+          <Message
+            v-if="$form.hostname?.invalid"
+            severity="error"
+            size="small"
+            variant="simple">
+            {{ $form.hostname.error.message }}
+          </Message>
+        </div>
+      </div>
+      <div class="flex items-center gap-4">
+        <label for="hostname" class="font-bold w-24 self-baseline">FQDN</label>
+        <InputText
+          id="hostname"
+          v-model="fqdn"
+          class="flex-auto h-8 p-0"
+          readonly
+          disabled
+        />
+      </div>
+      <div class="flex items-center gap-4">
+        <label for="ipAddress" class="font-bold w-24 self-baseline">IP Address</label>
+        <div class="flex flex-col flex-auto">
+          <InputText
+            v-model="ipAddress"
+            id="ipAddress"
+            name="ipAddress"
+            class="flex-auto h-8 p-0"
+            autocomplete="off"
+          />
+          <Message
+            v-if="$form.ipAddress?.invalid"
+            severity="error"
+            size="small"
+            variant="simple">
+            {{ $form.ipAddress.error.message }}
+          </Message>
+        </div>
+      </div>
+      <div class="flex items-center gap-4">
+        <label for="addPtr" class="font-bold w-24">Add PTR record</label>
+        <Checkbox
+          v-model="addPtr"
+          id="addPtr"
+          binary
+        />
+      </div>
+      <Button
+        type="submit"
+        severity="secondary"
+        label="Confirm"
+      />
+    </Form>
+  </Dialog>
+</template>
